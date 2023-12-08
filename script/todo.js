97bb04e9c6c4e866d1abaff8eaf44a6d4da1f408
Sooraj <sooraj.bhooshan@waltr.in>
2023-12-08 14:54:46 +0530

    todo working

--- script/todo.js
+++ script/todo.js
@@ -2,21 +2,29 @@ const todoList = [];
 
 function addTODO() {
 
-    let inputElement = document.querySelector('.js-input-todo');
-    todoList.push(inputElement.value);
-    console.log(todoList);
+    let inputTODO = document.querySelector('.js-input-todo');
+    let inputDate = document.querySelector('.js-todo-date');
+    todoList.push({ todo: inputTODO.value, date: inputDate.value });
     display();
 
 }
 
+
 function display() {
     let resultUI = ``;
     for (let i = 0; i < todoList.length; i++) {
-        resultUI += `<p>${todoList[i]} </p>`
+        resultUI +=
+            `<p>
+        ${todoList[i].todo}
+        ${todoList[i].date}
+         <button onclick="todoList.splice(${i},1);   display();" >Delete</button>
+        </p>`
     }
     console.log('-----------------');
     console.log(resultUI);
 
     document.querySelector('.js-result').innerHTML = resultUI;
 }
+
+
 
